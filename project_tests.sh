dea6e55a86e198d40be0f7e51dfe13c3bdb67182
nlisgo <n.lisgo@elifesciences.org>
2023-03-10 00:37:11 +0000

    Make improvements and expand tests

--- project_tests.sh
+++ project_tests.sh
@@ -6,41 +6,60 @@ TEST_FILE="$(mktemp)"
 # Get the directory where the script is located
 SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
 
-echo "Checking fixtures that have all xslt transforms applied (no doi match)"
-echo "----------------------------------------------------------------------"
+function transform_xml() {
+    if [ ! -s "${1}" ]; then
+        echo "Error: XML file is empty (${1})" >&2
+        exit 1
+    fi
+
+    if [ "${2:-}" = "--doi" ]; then
+        cat "${1}" | "${SCRIPT_DIR}/scripts/transform.sh" --doi "${3}"
+    else
+        if [ -n "$2" ] && [ ! -e "$2" ]; then
+            echo "Error: XSLT file is empty (${2})" >&2
+            exit 1
+        fi
+
+        cat "${1}" | "${SCRIPT_DIR}/scripts/transform.sh" "${2:-}"
+    fi
+}
+
+function expected() {
+    if diff -w -u "${1}" "${2}"; then
+        echo "Output matches expected (${3})"
+        echo ""
+    else
+        echo "Output does not match expected (${3})"
+        exit 1
+    fi
+}
+
+function section_title() {
+    echo "${1}"
+    printf '%*s\n' "${#1}" | tr ' ' '-'
+}
+
+section_title "Checking fixtures that have all xslt transforms applied (no doi match)"
 for xml_file in ${SCRIPT_DIR}/test/all/*.xml; do
     xml_filename=$(basename "${xml_file}")
     if [ ! -d "${SCRIPT_DIR}/src/${xml_filename%.*}" ]; then
         echo "Running test for (${xml_file})"
-        cat "${SCRIPT_DIR}/test/fixtures/$(basename ${xml_file})" | "${SCRIPT_DIR}/scripts/transform.sh" > "${TEST_FILE}"
+        transform_xml "${SCRIPT_DIR}/test/fixtures//$(basename ${xml_file})" > "${TEST_FILE}"
 
-        if diff -w -u "${TEST_FILE}" "${xml_file}"; then
-            echo "Output matches expected (${xml_file})"
-            echo ""
-        else
-            echo "Output does not match expected (${xml_file})"
-            exit 1
-        fi
+        expected "${TEST_FILE}" "${xml_file}" "${xml_file}"
     fi
 done
 
-echo "Checking xslt files that apply to all manuscripts"
-echo "-------------------------------------------------"
+section_title "Checking xslt files that apply to all manuscripts"
 for xsl_file in ${SCRIPT_DIR}/src/*.xsl; do
     xsl_filename=$(basename "${xsl_file}")
 
     if [ -d "${SCRIPT_DIR}/test/${xsl_filename%.*}" ]; then
         echo "Running tests for (${xsl_file})"
         for xml_file in ${SCRIPT_DIR}/test/${xsl_filename%.*}/*.xml; do
-            cat "${SCRIPT_DIR}/test/fixtures/$(basename ${xml_file})" | "${SCRIPT_DIR}/scripts/transform.sh" "${xsl_file}" > "${TEST_FILE}"
+            transform_xml "${SCRIPT_DIR}/test/fixtures/$(basename ${xml_file})" "${xsl_file}" > "${TEST_FILE}"
 
-            if diff -w -u "${TEST_FILE}" "${xml_file}"; then
-                echo "Output matches expected ($(basename ${xml_file}) - ${xsl_filename})"
-                echo ""
-            else
-                echo "Output does not match expected ($(basename ${xml_file}) - ${xsl_filename})"
-                exit 1
-            fi
+            expected "${TEST_FILE}" "${xml_file}" "$(basename ${xml_file}) - ${xsl_filename}"
         done
     else
         echo "No tests exist for (${xsl_file})"
@@ -48,43 +67,28 @@ for xsl_file in ${SCRIPT_DIR}/src/*.xsl; do
     fi
 done
 
-echo "Checking fixtures that have all xslt transforms applied (doi match)"
-echo "-------------------------------------------------------------------"
+section_title "Checking fixtures that have all xslt transforms applied (doi match)"
 for manuscript_dir in ${SCRIPT_DIR}/src/*/; do
     manuscript_doi=$(basename "${manuscript_dir}")
     for xml_file in ${SCRIPT_DIR}/test/all/${manuscript_doi}*.xml; do
         echo "Running test for (${xml_file})"
-        # todo: need to pass the manuscript id to transform.sh so we can apply all of the relevant styles
-        cat "${SCRIPT_DIR}/test/fixtures/$(basename ${xml_file})" | "${SCRIPT_DIR}/scripts/transform.sh" --doi "${manuscript_doi}" > "${TEST_FILE}"
+        transform_xml "${SCRIPT_DIR}/test/fixtures/$(basename ${xml_file})" --doi "${manuscript_doi}" > "${TEST_FILE}"
 
-        if diff -w -u "${TEST_FILE}" "${xml_file}"; then
-            echo "Output matches expected (${xml_file})"
-            echo ""
-        else
-            echo "Output does not match expected (${xml_file})"
-            exit 1
-        fi
+        expected "${TEST_FILE}" "${xml_file}" "${xml_file}"
     done
 done
 
-echo "Checking xslt files that apply to specific manuscripts"
-echo "------------------------------------------------------"
+section_title "Checking xslt files that apply to specific manuscripts"
 for xsl_file in ${SCRIPT_DIR}/src/*/*.xsl; do
     xsl_filename=$(basename "${xsl_file}")
     xsl_file_dir=$(basename $(dirname "${xsl_file}"))
 
     if [ -d "${SCRIPT_DIR}/test/${xsl_file_dir}/${xsl_filename%.*}" ]; then
-        echo "Running tests for (${xsl_file_dir}/${xsl_file})"
+        echo "Running tests for (${xsl_file})"
         for xml_file in ${SCRIPT_DIR}/test/${xsl_file_dir}/${xsl_filename%.*}/*.xml; do
-            cat "${SCRIPT_DIR}/test/fixtures/$(basename ${xml_file})" | "${SCRIPT_DIR}/scripts/transform.sh" "${xsl_file}" > "${TEST_FILE}"
+            transform_xml "${SCRIPT_DIR}/test/fixtures/$(basename ${xml_file})" "${xsl_file}" > "${TEST_FILE}"
 
-            if diff -w -u "${TEST_FILE}" "${xml_file}"; then
-                echo "Output matches expected ($(basename ${xml_file}) - ${xsl_file_dir}/${xsl_filename})"
-                echo ""
-            else
-                echo "Output does not match expected ($(basename ${xml_file}) - ${xsl_file_dir}/${xsl_filename})"
-                exit 1
-            fi
+            expected "${TEST_FILE}" "${xml_file}" "$(basename ${xml_file}) - ${xsl_file_dir}/${xsl_filename}"
         done
     else
         echo "No tests exist for (${xsl_file})"
