294e3eb7a547d8d4c15b64186034be0bb93fdb20
nlisgo <n.lisgo@elifesciences.org>
2023-03-11 16:48:24 +0000

    Add first draft of documentation with check

---
+++ XSL_DOCUMENTATION.md
@@ -0,0 +1,12 @@
+# Documentation justifying each XSL file
+
+Must contain an entry for each XSL file justifying the need for this. The more detail the better.
+
+Documentation should include recommendations for how each XSL file could ultimately be removed by one of:
+- improved biorXiv XML
+- better handling of XML in Encoda
+- better support for Encoda output in EPP client
+
+Please provide links to github issues which express these requirements.
+
+In the entry for each XSL file please link to the file in github.
---
+++ scripts/verify-documentation.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+# Get the directory where the script is located
+SCRIPT_DIR="$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")"
+PARENT_DIR="$(dirname "${SCRIPT_DIR}")"
+DOCUMENTATION_FILE="${PARENT_DIR}/XSL_DOCUMENTATION.md"
+DOCUMENTATION_ERROR=false
+
+verify_docs () {
+    local xsl_file="${1}"
+    local xsl_file_short="src/${xsl_file#*src/}"
+    local line_number=$(grep -n "${xsl_file_short}" "${DOCUMENTATION_FILE}" | cut -d ':' -f 1 | head -n1)
+
+    if [[ "${line_number}" -eq "" ]]; then
+        DOCUMENTATION_ERROR=true
+        echo "${xsl_file_short}: no documentation entry found"
+    else
+        echo "${xsl_file_short}: documentation entry found on line number ${line_number}"
+    fi
+}
+
+for xsl_file in $(find "${PARENT_DIR}/src" -type f -name '*.xsl'); do
+    verify_docs "${xsl_file}"
+done
+
+echo ""
+
+if [[ "${DOCUMENTATION_ERROR}" = true ]]; then
+    echo "Error: documentation not found for one or more XSL files in ${DOCUMENTATION_FILE}"
+    exit 1
+else
+    echo "Documentation contains an entry for all xsl files in ${DOCUMENTATION_FILE}"
+fi
